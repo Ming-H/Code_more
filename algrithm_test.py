090312afe0629d0726f57149615e97527ce04638
Ming <1518246548@qq.com>
2018-03-08 16:59:25 +0800

    Update algrithm_test.py

--- algrithm_test.py
+++ algrithm_test.py
@@ -256,6 +256,57 @@ def func():
         n = n**0.5
     print('%.2f' % sum)
 
+8、袋鼠过河
+一只袋鼠要从河这边跳到河对岸，河很宽，但是河中间打了很多桩子，每隔一米就有一个，每个桩子上都有一个弹簧，
+袋鼠跳到弹簧上就可以跳的更远。每个弹簧力量不同，用一个数字代表它的力量，如果弹簧力量为5，就代表袋鼠下一跳最多能够跳5米，
+如果为0，就会陷进去无法继续跳跃。河流一共N米宽，袋鼠初始位置就在第一个弹簧上面，要跳到最后一个弹簧之后就算过河了，
+给定每个弹簧的力量，求袋鼠最少需要多少跳能够到达对岸。如果无法到达输出-1
+输入描述:
+输入分两行，第一行是数组长度N (1 ≤ N ≤ 10000)，第二行是每一项的值，用空格分隔。
+输出描述:
+输出最少的跳数，无法到达输出-1
+示例1
+输入
+5
+2 0 1 1 1
+输出
+4
+
+def func():
+    N = int(input())
+    L = list(map(int, input().split()))
+    sum = L[0]
+    left = 0  
+    right = L[0]
+    count = 1
+    while sum < N:  
+        max_num = max(L[left + 1 : right+1])
+        if max_num == 0:
+            break
+        left = L.index(max_num)
+        right = left + max_num
+        sum += max_num
+        count += 1
+    if count == 1:
+        print(-1)
+    else:
+        print(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 参考文献：
 https://github.com/taizilongxu/interview_python#29-super-init
 http://www.cnblogs.com/ChenxofHit/archive/2011/03/18/1988431.html
