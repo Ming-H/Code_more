d0335a58778be062ea84d2dd626b3fec75dbf4d2
Ming <1518246548@qq.com>
2018-03-08 15:36:42 +0800

    Update algrithm_test.py

--- algrithm_test.py
+++ algrithm_test.py
@@ -237,6 +237,24 @@ def func():
     else:
         print(" ".join(map(str, num_list)))
 
+        n, m = map(int, input().split())
+
+7、求数列的和
+数列的第一项为n，以后各项为前一项的平方根，求数列的前m项的和。
+输入描述:
+输入数据有多组，每组占一行，由两个整数n（n < 10000）和m(m < 1000)组成，n和m的含义如前所述。
+输出描述:
+对于每组输入数据，输出该数列的和，每个测试实例占一行，要求精度保留2位小数。
+示例1
+输入
+81 4
+2 2
+def func():
+    sum = 0
+    for i in range(m):
+        sum += n
+        n = n**0.5
+    print('%.2f' % sum)
 
 参考文献：
 https://github.com/taizilongxu/interview_python#29-super-init
