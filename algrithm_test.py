890991acd9204587549f7b4d1d04906f6b8a538d
Ming <1518246548@qq.com>
2018-03-09 11:26:51 +0800

    Update algrithm_test.py

--- algrithm_test.py
+++ algrithm_test.py
@@ -323,6 +323,30 @@ def func():
         print(L-min-1)
     except:
         break
+        
+ 10、统计字符
+给定一个英文字符串,请写一段代码找出这个字符串中首先出现三次的那个英文字符。
+输入描述:
+输入数据一个字符串，包括字母,数字等。
+输出描述:
+输出首先出现三次的那个英文字符
+示例1
+输入
+Have you ever gone shopping and
+输出
+e
+def func():
+    num = input()
+    d = {}
+    for item in num:
+        if item.isalpha() is True:
+            if item not in d:
+                d[item] = 1
+            else:
+                d[item] += 1
+                if d[item] >=3:
+                    print(item)
+                    break
 
 
 
